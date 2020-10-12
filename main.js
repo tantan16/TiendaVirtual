93f6e61d78dd4399a8c2449d9b3e13cb90da09d1
tantan16 <tania.elizalde.vargas@gmail.com>
2020-10-11 23:53:33 -0500

    shopping_cart_added

---
+++ main.js
@@ -0,0 +1,15 @@
+// shopping chart code 
+const openShoppingCartMenu = document.getElementById("shopping-cart-button")
+const closeShoppingCartMenu = document.getElementById("close-menu")
+const shoppingCartMenu = document.getElementById("menu-shopping-cart")
+const overlay = document.getElementById("overlay")
+
+    openShoppingCartMenu.onclick = () => {
+        overlay.classList.remove("hidden")
+        shoppingCartMenu.classList.remove("hidden")
+    }
+
+    closeShoppingCartMenu.onclick = () =>{
+        overlay.classList.add("hidden")
+        shoppingCartMenu.classList.add("hidden")
+    }
